e3984ebbff3d8110774b09ce430e93195b716ddc
saqlain1020 <saqlainprinters@gmail.com>
2021-04-05 04:05:24 -0700

    Tests added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,58 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from "@testing-library/react";
+// import App from "./App";
+import { checarHabitaciones } from "./Functions/Functions";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let Premium;
+let Economy;
+const clients = [23, 45, 155, 374, 22, 99, 100, 101, 115, 209];
+
+describe("Test 1", () => {
+  test("Free Premium Rooms: 3 Free Economy Rooms: 3", () => {
+    Premium = 3;
+    Economy = 3;
+    expect(checarHabitaciones(Premium, Economy, clients)).toStrictEqual([
+      738,
+      167,
+    ]);
+  });
 });
+
+describe("Test 2", () => {
+  test("Free Premium Rooms: 6 Free Economy Rooms: 4", () => {
+    Premium = 6;
+    Economy = 4;
+    expect(checarHabitaciones(Premium, Economy, clients)).toStrictEqual([
+      1054,
+      189,
+    ]);
+  });
+});
+
+describe("Test 3", () => {
+  test("Free Premium Rooms: 2 Free Economy Rooms: 4", () => {
+    Premium = 2;
+    Economy = 4;
+    expect(checarHabitaciones(Premium, Economy, clients)).toStrictEqual([
+      583,
+      189,
+    ]);
+  });
+});
+
+describe("Test 4", () => {
+  test("Free Premium Rooms: 7 Free Economy Rooms: 1", () => {
+    Premium = 7;
+    Economy = 1;
+    expect(checarHabitaciones(Premium, Economy, clients)).toStrictEqual([
+      1153,
+      45,
+    ]);
+  });
+});
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   screen.getByText('')
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
--- src/Functions/Functions.js
+++ src/Functions/Functions.js
@@ -36,38 +36,3 @@ export function checarHabitaciones(Premium, Economy, clients) {
   let totalEarnings = [premiumProfits, economyProfits];
   return totalEarnings;
 }
-
-//   let Premium;
-//   let Economy;
-
-//   describe("Prueba 1", () => {
-//     test("Habitaciones gratuitas Premium: 3 Habitaciones gratuitas Economy: 3",() => {
-//       Premium = 3;
-//       Economy = 3;
-//       expect(checarHabitaciones(Premium, Economy)).toStrictEqual([738, 167]);
-//     });
-//   });
-
-//   describe("Prueba 2", () => {
-//     test("Habitaciones Premium gratuitas: 6 Habitaciones Economy gratuitas: 4",() => {
-//       Premium = 6;
-//       Economy = 4;
-//       expect(checarHabitaciones(Premium, Economy)).toStrictEqual([1054, 189]);
-//     });
-//   });
-
-//   describe("Prueba 3", () => {
-//     test("Habitaciones Premium gratuitas: 2 Habitaciones Economy gratuitas: 4",() => {
-//       Premium = 2;
-//       Economy = 4;
-//       expect(checarHabitaciones(Premium, Economy)).toStrictEqual([583, 189]);
-//     });
-//   });
-
-//   describe("Prueba 4", () => {
-//     test("Habitaciones Premium gratuitas: 7 Habitaciones Economy gratuitas: 1",() => {
-//       Premium = 7;
-//       Economy = 1;
-//       expect(checarHabitaciones(Premium, Economy)).toStrictEqual([1153, 45]);
-//     });
-//   });
