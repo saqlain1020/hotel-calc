4ac96212f117b3b9c952a5e6a4696b4c1642c302
saqlain1020 <saqlainprinters@gmail.com>
2021-04-05 04:17:24 -0700

    Tests updates

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ let Economy;
 const clients = [23, 45, 155, 374, 22, 99, 100, 101, 115, 209];
 
 describe("Test 1", () => {
-  test("Free Premium Rooms: 3 Free Economy Rooms: 3", () => {
+  test("Free Premium rooms: 3 Free Economy rooms: 3", () => {
     Premium = 3;
     Economy = 3;
     expect(checarHabitaciones(Premium, Economy, clients)).toStrictEqual([
@@ -18,7 +18,7 @@ describe("Test 1", () => {
 });
 
 describe("Test 2", () => {
-  test("Free Premium Rooms: 6 Free Economy Rooms: 4", () => {
+  test("Free Premium rooms: 6 Free Economy rooms: 4", () => {
     Premium = 6;
     Economy = 4;
     expect(checarHabitaciones(Premium, Economy, clients)).toStrictEqual([
@@ -29,7 +29,7 @@ describe("Test 2", () => {
 });
 
 describe("Test 3", () => {
-  test("Free Premium Rooms: 2 Free Economy Rooms: 4", () => {
+  test("Free Premium rooms: 2 Free Economy rooms: 4", () => {
     Premium = 2;
     Economy = 4;
     expect(checarHabitaciones(Premium, Economy, clients)).toStrictEqual([
@@ -40,7 +40,7 @@ describe("Test 3", () => {
 });
 
 describe("Test 4", () => {
-  test("Free Premium Rooms: 7 Free Economy Rooms: 1", () => {
+  test("Free Premium rooms: 7 Free Economy rooms: 1", () => {
     Premium = 7;
     Economy = 1;
     expect(checarHabitaciones(Premium, Economy, clients)).toStrictEqual([
